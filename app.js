b076eeeb4093c20527f62570b79a6871aa2c7540
Mohammad <mfalahat2002@gmail.com>
2024-02-13 22:42:20 +0330

    Update js file

--- app.js
+++ app.js
@@ -21,4 +21,81 @@ input.addEventListener('input', function() {
     }
 });
 
+// Select the input element for the cardholder's name
+let inputName = document.querySelector('.form-base #cardholder');
+
+// Select the 'p' element inside the 'card-name' div
+let cardName = document.querySelector('.card-name p');
+
+// Add an event listener to the input field for the cardholder's name
+inputName.addEventListener('input', function() {
+    // Get the value of the input field
+    let inputValue = this.value.toUpperCase(); // Convert to uppercase
+
+    // Update the cardName text
+    if (inputValue) {
+        cardName.textContent = inputValue;
+    } else {
+        // If the input is empty, set the default name
+        cardName.textContent = 'JANE APPLESEED';
+    }
+});
+
+// Select the input elements for the expiration month and year
+let inputMonth = document.querySelector('.namedate-part-left div:nth-child(1) input');
+let inputYear = document.querySelector('.namedate-part-left div:nth-child(2) input');
+
+// Select the 'p' element inside the 'card-exp-date' div
+let cardExpDate = document.querySelector('.card-exp-date p');
+
+// Add an event listener to the input field for the expiration month
+inputMonth.addEventListener('input', function() {
+    // Get the value of the input field
+    let inputMonthValue = this.value;
+
+    // Get the current value of the card's expiration date
+    let currentExpDate = cardExpDate.textContent.split('/');
+
+    // Update the month in the card's expiration date
+    currentExpDate[0] = inputMonthValue ? inputMonthValue.padStart(2, '0') : '00';
+
+    // Update the card's expiration date
+    cardExpDate.textContent = currentExpDate.join('/');
+});
+
+// Add an event listener to the input field for the expiration year
+inputYear.addEventListener('input', function() {
+    // Get the value of the input field
+    let inputYearValue = this.value;
+
+    // Get the current value of the card's expiration date
+    let currentExpDate = cardExpDate.textContent.split('/');
+
+    // Update the year in the card's expiration date
+    currentExpDate[1] = inputYearValue ? inputYearValue.padStart(2, '0') : '00';
+
+    // Update the card's expiration date
+    cardExpDate.textContent = currentExpDate.join('/');
+});
+
+// Select the input element for the CVC
+let inputCVC = document.querySelector('.namedate-part-right input');
+
+// Select the 'p' element inside the 'card-cvc' div
+let cardCVC = document.querySelector('.card-cvc p');
+
+// Add an event listener to the input field for the CVC
+inputCVC.addEventListener('input', function() {
+    // Get the value of the input field
+    let inputCVCValue = this.value;
+
+    // Update the cardCVC text
+    if (inputCVCValue) {
+        cardCVC.textContent = inputCVCValue.padStart(3, '0');
+    } else {
+        // If the input is empty, set the default CVC
+        cardCVC.textContent = '000';
+    }
+});
+
 
